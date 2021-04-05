058b090f584d563ff6cce4dc8e0c6e2189667515
martamajewska <marta.majewska@yougov.com>
2021-04-05 16:00:07 +0200

    add task component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,8 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../src/app/components/**/*.stories.ts',
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ]

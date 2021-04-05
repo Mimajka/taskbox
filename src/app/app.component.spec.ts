da6208b6f0bbe46b2397177b02d384bbd684e26f
martamajewska <marta.majewska@yougov.com>
2021-04-05 19:29:45 +0200

    add inbox screen

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,23 @@
-import { render } from '@testing-library/angular';
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {async, TestBed} from '@angular/core/testing';
+import {NgxsModule, Store} from '@ngxs/store';
+import {TaskModule} from './components/task.module';
+import {TasksState} from './state/task.state';
+import {InboxScreenComponent} from './components/inbox-screen-component';
+import {PureInboxScreenComponent} from './components/pure-inbox-screen.component';
 
 describe('App', () => {
-  test('should render the App component', async () => {
-    const { getByText } = await render(AppComponent);
-    expect(getByText('Welcome'));
-  });
-})
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent, InboxScreenComponent, PureInboxScreenComponent],
+      imports: [TaskModule, NgxsModule.forRoot([TasksState])],
+      providers: [Store],
+    }).compileComponents();
+  }));
+
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
+});
